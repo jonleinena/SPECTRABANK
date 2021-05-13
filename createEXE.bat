570c67acafe7ceb4316656e969c988b01a67fcf3
Alvaro Gutierrez Gonzalez <alvaro3639@gmail.com>
2021-05-14 01:08:56 +0200

    created batch file for compilation

---
+++ createEXE.bat
@@ -0,0 +1,9 @@
+gcc lib/sqlite3/sqlite3.c db/dbConnection.c db/C/getData.c db/C/postData.c main/C/login/loginProfesional.c main/C/menuProfesional/menuProfesional.c main/C/utils/fechas.c main/C/utils/prestamo/prestamo.c -c
+
+g++ main/main.cpp .\db\C++\getDataCPP.cpp main/C++/login/loginCliente.cpp main/C++/utils/containers/containers.cpp main/C++/menuCliente/menuCliente.cpp -c
+
+g++ containers.o dbConnection.o fechas.o getData.o getDataCPP.o loginCliente.o .\loginProfesional.o .\main.o .\menuCliente.o .\menuProfesional.o .\postData.o .\prestamo.o .\sqlite3.o -o main
+
+del *.o
+
+.\main
